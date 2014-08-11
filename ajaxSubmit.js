00616d53838466cbc2a3862638452f06b7552f07
craigcosmo <craigcosmo@gmail.com>
2014-08-11 13:56:13 +0700

    update

--- ajaxSubmit.js
+++ ajaxSubmit.js
@@ -2,13 +2,12 @@
 	$.fn.ajaxSubmit = function(options){
 		var defaults = {
 			url:'',
-			submitButton:'.submitBtn',
+			preventSubmit:'submiting',
 			dataType:'json',
 			onSubmit:function(){return true;},
 			onSuccess:function(i){},
 			onError:function(i){},
 			data:'',
-			varData:'',
 			resubmisable:false
 		},
 		o = $.extend({},defaults, options);
@@ -16,86 +15,90 @@
 		return this.each(function(){
 			var $this=$(this);
 			var submisable= true;
-			var button = $this.find(o.submitButton);
+			var data ='';
+			var isForm = $this.is('form') ==true ? true : false;
 			//console.log($this.attr('class'));
-			
-			if($this.is('form')){
+
+
+			var send = function(){
+				$this.addClass(o.preventSubmit);
+				$this.click(function(e){e.preventDefault();});//if element is a link
+
+				o.onSubmit.call(this);
+				$.post(o.url,o.data ,
+				function(i){
+					if(i.status == 'success' && o.dataType == 'json'){
+						o.onSuccess.call(this, i);
+						if(o.resubmisable){$this.removeClass(o.preventSubmit);}
+					}
+					else if(i.status == 'error' && o.dataType == 'json'){
+						o.onError.call(this, i);
+						$this.removeClass(o.preventSubmit);
+					}
+					else{	
+						o.onSuccess.call(this, i);
+						if(o.resubmisable){$this.removeClass(o.preventSubmit);}
+					}
+				},o.dataType);
+			};
+
+
+			if(isForm){
+
 				$this.submit(function(e){
 					e.preventDefault();
-					if(submisable){
-						submisable = false;
-						if(o.onSubmit.call(this)==false){
-							submisable =true;
-							return;				
-						}
-						var data='';
-						if(o.data){
-							data = $.param(o.data)+'&';
-						}
-						var varData='';
-						if(o.varData){
-							var object = {};
-							$.each(o.varData, function(key, el) {
-								var v = $(el).html();
-								object[key]=v; 
-							});
-							varData = $.param(object)+'&';	
-						}
-						
-						$.post(o.url,varData + data + $this.serialize() ,
-						function(i){
-							// check if returned data is json
-							if(i.status == 'ok' && o.dataType == 'json'){
-								o.onSuccess.call(this, i);
-							}
-							else if(i.status == 'error' && o.dataType == 'json'){
-								o.onError.call(this, i);
-								submisable =true;
-							}
-							else{
-								o.onSuccess.call(this, i);
-							}
-						},o.dataType);
-					}
-				});
-				
-				$this.find('input[type="text"], input[type="password"]').keyup(function(e){
-					if (e.which == 13 || e.which == 10 ) {
-						if(submisable){$this.submit();}
-					}else{
+					if(!submisable) return;
+					submisable = false;
+					
+					if(o.onSubmit.call(this)==false){
 						submisable =true;
+						return;				
 					}
-				});
-				$this.find('input[type="checkbox"], input[type="radio"], textarea, select').bind('doit',function(){
-					submisable = true;
-				});
-				$this.on('click', button,function(e){
-					button.closest('form').submit();
-				});
-				
-			}
-			else{
-				if(!$this.hasClass('submiting')){
-					$this.addClass('submiting');
-					$this.click(function(e){e.preventDefault();});//if element is a link
-					o.onSubmit.call(this);
-					$.post(o.url,o.data ,
+					if(o.data){data = $.param(o.data)+'&';}
+					$.post(o.url, data + $this.serialize() ,
 					function(i){
-						if(i.status == 'ok' && o.dataType == 'json'){
+						if(i.status == 'success' && o.dataType == 'json'){
 							o.onSuccess.call(this, i);
-							// alert('d');
-							if(o.resubmisable){$this.removeClass('submiting');}
 						}
 						else if(i.status == 'error' && o.dataType == 'json'){
 							o.onError.call(this, i);
-							$this.removeClass('submiting');
-						}
+							submisable =true;
+						}// incase server don't return error or success message, but the response is complete
 						else{
 							o.onSuccess.call(this, i);
-							if(o.resubmisable){$this.removeClass('submiting');}
+							// console.log('after submit = '+submisable);
 						}
 					},o.dataType);
-				}
+				});
+
+
+				$this.find('input[type="text"], input[type="password"]').keyup(function(e){
+					// console.log('key');
+					// e.preventDefault();
+					// weird case: the enter will always submit the form even with e.prevendefault. even when not call submit()
+					// it would still submit. therefore only set submissable to true if the keycode is not 13
+					if(e.which != 13){ submisable =true; console.log('the sub 13');}
+					// if (e.which == 13 && submisable) { console.log('keyup');} //$this.submit();}
+					// e.preventDefault();
+				});
+				$this.find('textarea').keyup(function(e){
+					// ctrl + enter to submit
+					if(e.ctrlKey && e.which == 13 && submisable){$this.submit();}
+					// or cmd + enter to submit
+					else if(e.which == 224 && e.which == 13 && submisable){$this.submit();}
+				});
+				$this.find('input[type="checkbox"], input[type="radio"],select').change(function(e){
+					submisable =true;
+				});
+
+				// this for rare case that needs resubmit although the fields haven't changed at all
+				// $this.find('input[type="checkbox"], input[type="radio"], textarea, select').bind('doit',function(){
+				// 	submisable = true;
+				// });
+				console.log('before submit = '+ submisable);
+			}
+			if(!isForm && !$this.hasClass(o.preventSubmit)){
+				send();
 			}
 						
 			$.fn.ajaxSubmit.resubmisable= function(targetForm) {
