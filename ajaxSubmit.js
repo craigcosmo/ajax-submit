cd3c0c2c63ec0e5359f358aa76556b165f0f4fb2
craigcosmo <craigcosmo@gmail.com>
2015-08-25 21:58:03 +0700

    updates

--- ajaxSubmit.js
+++ ajaxSubmit.js
@@ -14,7 +14,6 @@
 		
 		return this.each(function(){
 			var $this=$(this);
-			var submisable= true;
 			var data ='';
 			var isForm = $this.is('form') ==true ? true : false;
 			//console.log($this.attr('class'));
@@ -47,11 +46,14 @@
 
 				$this.submit(function(e){
 					e.preventDefault();
-					if(!submisable) return;
-					submisable = false;
+					if($this.hasClass(o.preventSubmit)){ 
+						console.log('can not submit');
+						return;
+					}
+					$this.addClass(o.preventSubmit);
 					
 					if(o.onSubmit.call(this)==false){
-						submisable =true;
+						$this.removeClass(o.preventSubmit);
 						return;				
 					}
 					if(o.data){data = $.param(o.data)+'&';}
@@ -59,55 +61,56 @@
 					function(i){
 						if(i.status == 'success' && o.dataType == 'json'){
 							o.onSuccess.call(this, i);
+							if(o.resubmisable == true){
+								$this.removeClass(o.preventSubmit);
+							}
 						}
 						else if(i.status == 'error' && o.dataType == 'json'){
 							o.onError.call(this, i);
-							submisable =true;
+							$this.removeClass(o.preventSubmit);
 						}// incase server don't return error or success message, but the response is complete
 						else{
 							o.onSuccess.call(this, i);
-							// console.log('after submit = '+submisable);
+							$this.removeClass(o.preventSubmit);
 						}
 					},o.dataType);
 				});
 
 
-				$this.find('input[type="text"], input[type="password"]').keyup(function(e){
+				$this.find('input[type="text"], input[type="password"]', 'textarea').keyup(function(e){
 					// console.log('key');
 					// e.preventDefault();
 					// weird case: the enter will always submit the form even with e.prevendefault. even when not call submit()
 					// it would still submit. therefore only set submissable to true if the keycode is not 13
-					if(e.which != 13){ submisable =true; console.log('the sub 13');}
+					if(e.which != 13){ $this.removeClass(o.preventSubmit);}
 					// if (e.which == 13 && submisable) { console.log('keyup');} //$this.submit();}
 					// e.preventDefault();
 				});
-				$this.find('textarea').keyup(function(e){
-					// ctrl + enter to submit
-					if(e.ctrlKey && e.which == 13 && submisable){$this.submit();}
-					// or cmd + enter to submit
-					else if(e.which == 224 && e.which == 13 && submisable){$this.submit();}
-				});
+				// this to submit with cmd + enter key. currently not working
+				// $this.find('textarea').keyup(function(e){
+				// 	// ctrl + enter to submit
+				// 	if(e.ctrlKey && e.which == 13 && submisable){$this.submit();}
+				// 	// or cmd + enter to submit
+				// 	else if(e.which == 224 && e.which == 13 && submisable){$this.submit();}
+				// });
+
+				// this listen to change of those element, if they change, allow user to submit again
 				$this.find('input[type="checkbox"], input[type="radio"],select').change(function(e){
-					submisable =true;
+					$this.removeClass(o.preventSubmit);
 				});
 
 				// this for rare case that needs resubmit although the fields haven't changed at all
 				// $this.find('input[type="checkbox"], input[type="radio"], textarea, select').bind('doit',function(){
 				// 	submisable = true;
 				// });
-				console.log('before submit = '+ submisable);
+
 			}
 			if(!isForm && !$this.hasClass(o.preventSubmit)){
 				send();
 			}
-						
-			$.fn.ajaxSubmit.resubmisable= function(targetForm) {
-				$(targetForm).find('input[type="checkbox"], input[type="radio"], textarea, select').trigger('doit');
-				$(targetForm).find('input[type="text"], input[type="password"]').keyup();
-			};
-			
-
+									
 		});
 	}
+	//  never use this plugin on object document, because it will cause the bug that prevent links working. Which is clicking on the link will go anywhere
 	
 })(jQuery);
